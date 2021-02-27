6262f5b78fdb1a6e413c59f4f0859d8f5121df37
Luís Duarte <lduarte@luisduarte.net>
2021-02-27 17:45:04 +0000

    Fixed: ByteBuffer wasn't properly refreshed

--- rnc1/bitstream.go
+++ rnc1/bitstream.go
@@ -14,12 +14,12 @@ type BitStream struct {
 	bytesRead      int64
 	reader         *io.Reader
 
-	reachedEOF bool
-	bufferSize int32
-	byteBuffer []byte
+	reachedEOF       bool
+	bufferSize       int32
+	byteBuffer       []byte
 	currentByteIndex int32
-	consumedBytes int
-	lastReadBytes []byte
+	consumedBytes    int
+	lastReadBytes    []byte
 }
 
 func NewBitStream(reader io.Reader, initialPos int32, endPos int32) BitStream {
@@ -47,14 +47,16 @@ func NewBitStream(reader io.Reader, initialPos int32, endPos int32) BitStream {
 	return result
 }
 
-func (s *BitStream) RefreshBuffer()  {
+func (s *BitStream) RefreshBuffer() {
 	s.bitBufferCount -= 16
 	s.bitBuffer &= (1 << uint32(s.bitBufferCount)) - 1
 
-	if s.bytesRead < s.endOfData- 1 {
+	s.refreshByteBuffer()
+
+	if s.bytesRead < s.endOfData-1 {
 		s.bitBuffer |= s.readU16LE() << s.bitBufferCount
 		s.bitBufferCount += 16
-	} else if s.bytesRead == s.endOfData- 1 {
+	} else if s.bytesRead == s.endOfData-1 {
 		s.bitBuffer |= s.readByte() << s.bitBufferCount
 		s.bitBufferCount += 16
 	}
@@ -70,7 +72,7 @@ func (s *BitStream) Advance(bits int) {
 
 	s.advanceByteBufferIndex(2)
 
-	if s.bytesRead < s.endOfData- 1 {
+	if s.bytesRead < s.endOfData-1 {
 		s.bitBuffer |= s.readU16LE() << s.bitBufferCount
 		s.bitBufferCount += 16
 	} else if s.bytesRead < s.endOfData {
@@ -81,14 +83,15 @@ func (s *BitStream) Advance(bits int) {
 
 func (s *BitStream) advanceByteBufferIndex(length int32) {
 	s.currentByteIndex += length
-	if s.currentByteIndex > s.bufferSize {
+	s.bytesRead += int64(length)
+	// Ensure that there's at least 2 contiguous bytes in buffer available to read
+	if s.currentByteIndex+2 > s.bufferSize {
 		s.refreshByteBuffer()
-		s.currentByteIndex -= s.bufferSize
 	}
 }
 
 func (s *BitStream) readU16LE() uint32 {
-	result := s.byteBuffer[s.currentByteIndex : s.currentByteIndex + 2]
+	result := s.byteBuffer[s.currentByteIndex : s.currentByteIndex+2]
 	return uint32(core.ToUint16LE(result))
 }
 
@@ -110,34 +113,56 @@ func (s *BitStream) ReadBits(mask uint32, bits int) uint32 {
 }
 
 func (s *BitStream) readBytes(length int32) ([]byte, error) {
-	if s.currentByteIndex + length > s.bufferSize {
+	if s.currentByteIndex+length > s.bufferSize {
 		partialCount := s.currentByteIndex + length - s.bufferSize
-		partial := s.byteBuffer[s.currentByteIndex : partialCount]
+		partial := s.byteBuffer[s.currentByteIndex:partialCount]
 
 		s.advanceByteBufferIndex(partialCount)
 
 		remainingCount := length - partialCount
-		remaining := s.byteBuffer[s.currentByteIndex : remainingCount]
+		remaining := s.byteBuffer[s.currentByteIndex:remainingCount]
 
 		return append(partial, remaining...), nil
 	}
 
-	result := s.byteBuffer[s.currentByteIndex : s.currentByteIndex + int32(length)]
+	result := s.byteBuffer[s.currentByteIndex : s.currentByteIndex+length]
 	s.currentByteIndex += length
 
 	return result, nil
 }
 
-func (s *BitStream) needsBufferRefresh() bool {
-	return s.currentByteIndex >= s.bufferSize
+func (s *BitStream) needsBufferRefresh(bytesToRead int32) bool {
+	return s.currentByteIndex+bytesToRead > s.bufferSize
 }
 
 func (s *BitStream) refreshByteBuffer() (int, error) {
-	bytesRead, err := io.ReadFull(*s.reader, s.byteBuffer)
-	if err == io.ErrUnexpectedEOF {
-		s.reachedEOF = true
-		return bytesRead, nil
+	bytesStillAvailable := s.bufferSize - s.currentByteIndex
+
+	if bytesStillAvailable == s.bufferSize {
+		bytesStillAvailable = 0
 	}
 
-	return -1, err
+	var intermediateBuffer []byte
+
+	if bytesStillAvailable > 0 {
+		intermediateBuffer = s.byteBuffer[s.currentByteIndex:]
+	}
+
+	newBuffer := make([]byte, s.bufferSize-bytesStillAvailable)
+	bytesRead, err := io.ReadFull(*s.reader, newBuffer)
+
+	if intermediateBuffer != nil {
+		s.byteBuffer = append(intermediateBuffer, newBuffer...)
+	} else {
+		s.byteBuffer = newBuffer
+	}
+
+	s.currentByteIndex = 0
+
+	if err == io.EOF {
+		s.reachedEOF = true
+		return bytesRead + int(bytesStillAvailable), nil
+	}
+
+	return bytesRead, err
 }
